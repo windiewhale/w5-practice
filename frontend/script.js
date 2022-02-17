c9a266f13adfd8ae125c73ac53ec4dcc56533895
windiewhale <feketebodza@gmail.com>
2022-02-17 18:29:10 +0100

    onemore version

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,6 @@ function loadEvent() {
     let rootElement = document.getElementById("root");
 
     let card2 = function(title, year, rate){
-        
         return ` 
         <div class="card">
             <h3>${title}</h3>
@@ -16,54 +15,41 @@ function loadEvent() {
 
     let renderAllCardElements = function (incomingMoviesArray) {
         
-        let renderedCardList = ""; 
+        let renderedCardList = `<div class="cards">`; // ebbe akartuk beleírni a kódot azért kellett, js szempontból string ami itt van az gész ezért így lehet hogy a div eleje itt aztán a lezáró a for után adjuk hozzá
         //for ciklus ami végig megy a cardsArrayen
         for (const incomingMovie of incomingMoviesArray) {
+           renderedCardList += card2(incomingMovie.title, incomingMovie.year, incomingMovie.rate)
+           
             //minden lépcsőjénél hozzá adja a renderedCardList hez az adott elemet a megfeleő div cardban
-            renderedCardList  += ` 
+           /*  renderedCardList  += ` 
                 <div class="card">
                     <h3>${incomingMovie.title}</h3>
                     <div class="time">${incomingMovie.year}</div>
                     <div class="rate">${incomingMovie.rate}</div>
                 </div>
-                `
+                ` */
         }
+        renderedCardList += `</div>`; //cards elemeknek legyen lezáró tagje azért van itt
         console.log(renderedCardList);
         //returnöli az elékszült elemekkel feltöltött renderedCardListet
         return renderedCardList;
     }
 
-    /* movies.sort(function(a, b){return a.year - b.year}); //az egész adatot sorba rendezi, az eredetet */
     
-    let newGoodMovies = [];
+    let newGoodMovies = []; //ezt az arrayt töltjük fel a kövi sorokkal
 
     for (const movieSend of movies) { 
-        /* let newerThan2000 = false; 
-        
-        if (movieSend.year > 2000){ //csak akkor hívja meg ha ennek teljesül, 2000nél újabb
-            newerThan2000 = true;
-        }  
-
-        if (newerThan2000){
-        rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, movieSend.rate)); 
-        } */
-
-        // && még egy része teljesüljön az állításnak, Math.floor bekerülhet a movieSend.rate elé, akkr kerekíti, vagy function résznél (12. sor ${Math.floor(rate)} ) ahol behívjuk az adatot, vagy külön változót csinálunk (29), amit aztán a card2 paramétereként hívjuk meg 
-       // let floorRate = Math.floor(movieSend.rate);
-
+       
         if (movieSend.year > 2000 && movieSend.rate >= 8) {
-           newGoodMovies.push(movieSend); //így csak az kerül a tömbbe ami ezeknek a feltételeknek megfelel
-           // rootElement.insertAdjacentHTML("beforeend", card2(movieSend.title, movieSend.year, floorRate)); 
+           newGoodMovies.push(movieSend);
         }
-
     }
-    newGoodMovies.sort(function(a, b){return a.year - b.year}); //átrendeztük 
 
-    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies)); //elküldve újrarendezve 
+    newGoodMovies.sort(function(a, b){return a.year - b.year}); //az újonnan létrehozott tömböt rendezzük sorba
+
+    rootElement.insertAdjacentHTML("beforeend", renderAllCardElements(newGoodMovies)); //meghínjuk a rendereallcardselementset,jön a string, abban fut le for ciklus. 
     
 }
-
-
 
 window.addEventListener("load", loadEvent);
 
